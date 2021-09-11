6b33d015eb8056aca71837c093b1bd9db351eb4d
tooobeing <47571931+tooobeing@users.noreply.github.com>
2021-09-11 19:35:56 +0300

    adding files cont

---
+++ Preparescreen.m
@@ -0,0 +1,25 @@
+%This function prepares the parameter of the experiment
+function Parameter = Preparescreen()
+
+HideCursor;
+
+Screen('Preference', 'SkipSyncTests', 1);
+[Parameter.window, Parameter.rect] = Screen('OpenWindow', 0,[0,0,0]);
+Parameter.width = Parameter.rect(3);
+Parameter.heigth = Parameter.rect(4);
+Parameter.centerX = Parameter.rect(3)/2;
+Parameter.centerY = Parameter.rect(4)/2;
+
+Screen('TextFont', Parameter.window, 'Times');
+Screen('TextSize', Parameter.window, 60);
+
+%Keys that will be using during the experiment
+Parameter.yes = KbName('z');
+Parameter.no = KbName('m');
+Parameter.space = KbName('space');
+
+Parameter.color = [255,255,255];
+
+
+
+end
